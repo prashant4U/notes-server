1149eff5f3a4d759ca8b52274a169453995ac787
Prashant Dhanorkar <prashant.dhanorkar@healthpole.com>
2020-09-24 01:58:04 +0530

    made app as heroku json comatible

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const port = process.env.PORT || 4000
-const uri = "mongodb+srv://admin-psd:psd@mongodb-atlas@cluster0.dauig.mongodb.net/Blogs?retryWrites=true&w=majority"
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const uri = "mongodb+srv://admin-psd:psd@mongodb-atlas@cluster0.dauig.mongodb.net/Notes?retryWrites=true&w=majority"//"mongodb://localhost:27017/notesDB"
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
 }));
 
 app.use(express.static("public"));
@@ -25,141 +25,121 @@ app.use(express.static("public"));
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}, function(err) {
+  if (!err) console.log("Connection to mongoDB is Successful.");
+  else console.log("Connection to mongoDB is UnSuccessful.");
 });
 
-const postItemSchema = mongoose.Schema({
+const NoteItemSchema = mongoose.Schema({
   title: {
     type: String,
     required: [true, "Title cannot be blank!"]
   },
-  content: {
+  body: {
     type: String,
-    required: [true, "Description cannot be blank!"]
+    required: [true, "Body cannot be blank!"]
   }
 });
 
-const postItem = mongoose.model("Post", postItemSchema);
+const noteItem = mongoose.model("Note", NoteItemSchema);
 
 let posts = [];
 
-app.get("/", function(req, res) {
-  postItem.find({}, function(err, allPosts) {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/notes", function(req, res) {
+  noteItem.find({}, function(err, allNotes) {
     if (err) {
       console.log(err);
     } else {
       console.log("fetched all documents from DB!");
-      res.render("home", {
-        startingContent: homeStartingContent,
-        posts: allPosts
-      });
+
+      res.json(allNotes);
     }
   });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about", {
-    aboutContent: aboutContent
-  });
-});
 
-app.get("/contact", function(req, res) {
-  res.render("contact", {
-    contactContent: contactContent
-  });
-});
+app.post("/notes", function(req, res) {
+  let {title, body} = req.body;
+  console.log(req.body)
+  const note = new noteItem({title,body});
 
-app.get("/compose", function(req, res) {
-  res.render("compose", {
-    title: "",
-    content: "",
-    submitUrl: "/compose"
-  });
-});
-
-app.post("/compose", function(req, res) {
-  const post = new postItem({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-  postItem.insertMany([post], function(err, insertedRow) {
+  noteItem.insertMany([note], function(err, insertedRow) {
     if (err) {
       console.log(err);
     } else {
       console.log("document inserted successfully to DB!");
-      res.redirect("/");
+      res.json(insertedRow);
     }
   });
 });
 
-app.get("/posts/:postId", function(req, res) {
-  const postId = req.params.postId;
+app.get("/notes/:noteId", function(req, res) {
+  const noteId = req.params.noteId;
 
-  postItem.findOne({
-    _id: postId
-  }, function(err, post) {
+  noteItem.findOne({
+    _id: noteId
+  }, function(err, note) {
     if (err) {
       console.log(err);
     } else {
-      console.log(post);
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-        id: post._id
-      });
+      console.log(note);
+      res.json(note);
     }
   });
 });
 
-app.get("/posts/:postId/edit", function(req, res) {
-  const postId = req.params.postId;
+app.get("/notes/:noteId/edit", function(req, res) {
+  const noteId = req.params.noteId;
 
-  postItem.findOne({
-    _id: postId
-  }, function(err, post) {
+  noteItem.findOne({
+    _id: noteId
+  }, function(err, note) {
     if (err) {
       console.log(err);
     } else {
-      console.log(post);
-      res.render("compose", {
-        title: post.title,
-        content: post.content,
-        id: post._id,
-        submitUrl: "/posts/" + post._id + "/edit"
-      });
+      console.log(note);
+      res.json(note);
     }
   });
 });
 
-app.post("/posts/:postId/edit", function(req, res) {
-  const postId = req.params.postId;
-  const title = req.body.postTitle;
-  const content = req.body.postBody;
-  const action = req.body.btnAction;
+app.post("/notes/:noteId/edit", function(req, res) {
+  const noteId = req.params.noteId;
+  const title = req.body.title;
+  const content = req.body.body;
+  const action = req.body.btnAction ? req.body.btnAction : "Delete";
   console.log(action);
   if (action == "Update") {
-    postItem.updateOne({
-      _id: postId
+    noteItem.updateOne({
+      _id: noteId
     }, {
       title: title,
-      content: content
-    }, function(err, post) {
+      body: content
+    }, function(err, note) {
       if (err) {
         console.log(err);
       } else {
-        console.log("post updated sucessfully.");
-        res.redirect("/");
+        console.log("note updated sucessfully.");
+        //res.redirect("/");
+        res.json(note);
       }
     });
   } else if (action == "Delete") {
-    postItem.deleteOne({
-      _id: postId
-    }, function(err, post) {
+    noteItem.deleteOne({
+      _id: noteId
+    }, function(err, note) {
       if (err) {
         console.log(err);
       } else {
-        console.log("post deleted sucessfully.");
-        res.redirect("/");
+        console.log("note deleted sucessfully.");
+        //res.redirect("/");
+        res.json(note);
       }
     });
   }
